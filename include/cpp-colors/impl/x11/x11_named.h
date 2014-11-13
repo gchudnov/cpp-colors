6c1767d924690cbabacd08b0d0b3311ed82f1087
Grigoriy Chudnov <g.chudnov@gmail.com>
2014-11-14 00:58:27 +0300

    added some x11 color names

--- include/cpp-colors/impl/x11/x11_named.h
+++ include/cpp-colors/impl/x11/x11_named.h
@@ -28,66 +28,226 @@ namespace colors {
         return "PaleVioletRed";
       }
 
+      static const char* const medium_violet_red() {
+        return "MediumVioletRed";
+      }
 
-//MediumVioletRed	C7 15 85	199  21 133
-//Red colors
-//LightSalmon	FF A0 7A	255 160 122
-//Salmon	FA 80 72	250 128 114
-//DarkSalmon	E9 96 7A	233 150 122
-//LightCoral	F0 80 80	240 128 128
-//IndianRed	CD 5C 5C	205  92  92
-//Crimson	DC 14 3C	220  20  60
-//FireBrick	B2 22 22	178  34  34
-//DarkRed	8B 00 00	139   0   0
-//Red	FF 00 00	255   0   0
-//Orange colors
-//OrangeRed	FF 45 00	255  69   0
-//Tomato	FF 63 47	255  99  71
-//Coral	FF 7F 50	255 127  80
-//DarkOrange	FF 8C 00	255 140   0
-//Orange	FF A5 00	255 165   0
-//Yellow colors
-//Yellow	FF FF 00	255 255   0
-//LightYellow	FF FF E0	255 255 224
-//LemonChiffon	FF FA CD	255 250 205
-//LightGoldenrodYellow	FA FA D2	250 250 210
-//PapayaWhip	FF EF D5	255 239 213
-//Moccasin	FF E4 B5	255 228 181
-//PeachPuff	FF DA B9	255 218 185
-//PaleGoldenrod	EE E8 AA	238 232 170
-//Khaki	F0 E6 8C	240 230 140
-//DarkKhaki	BD B7 6B	189 183 107
-//Gold	FF D7 00	255 215   0
-//Brown colors
-//Cornsilk	FF F8 DC	255 248 220
-//BlanchedAlmond	FF EB CD	255 235 205
-//Bisque	FF E4 C4	255 228 196
-//NavajoWhite	FF DE AD	255 222 173
-//Wheat	F5 DE B3	245 222 179
-//BurlyWood	DE B8 87	222 184 135
-//Tan	D2 B4 8C	210 180 140
-//RosyBrown	BC 8F 8F	188 143 143
-//SandyBrown	F4 A4 60	244 164  96
-//Goldenrod	DA A5 20	218 165  32
-//DarkGoldenrod	B8 86 0B	184 134  11
-//Peru	CD 85 3F	205 133  63
-//Chocolate	D2 69 1E	210 105  30
-//SaddleBrown	8B 45 13	139  69  19
-//Sienna	A0 52 2D	160  82  45
-//Brown	A5 2A 2A	165  42  42
-//Maroon	80 00 00	128   0   0
+      static const char* const light_salmon() {
+        return "LightSalmon";
+      }
+
+      static const char* const salmon() {
+        return "Salmon";
+      }
+
+      static const char* const dark_salmon() {
+        return "DarkSalmon";
+      }
+
+      static const char* const light_coral() {
+        return "LightCoral";
+      }
+
+      static const char* const indian_red() {
+        return "IndianRed";
+      }
+
+      static const char* const crimson() {
+        return "Crimson";
+      }
+
+      static const char* const fire_brick() {
+        return "FireBrick";
+      }
+
+      static const char* const dark_red() {
+        return "DarkRed";
+      }
+
+      static const char* const red() {
+        return "Red";
+      }
+
+      static const char* const orange_red() {
+        return "OrangeRed";
+      }
+
+      static const char* const tomato() {
+        return "Tomato";
+      }
+
+      static const char* const coral() {
+        return "Coral";
+      }
+
+      static const char* const dark_orange() {
+        return "DarkOrange";
+      }
+
+      static const char* const orange() {
+        return "Orange";
+      }
+
+      static const char* const yellow() {
+        return "Yellow";
+      }
+
+      static const char* const light_yellow() {
+        return "LightYellow";
+      }
+
+      static const char* const lemon_chiffon() {
+        return "LemonChiffon";
+      }
+
+      static const char* const light_goldenrod_yellow() {
+        return "LightGoldenrodYellow";
+      }
+
+      static const char* const papaya_whip() {
+        return "PapayaWhip";
+      }
+
+      static const char* const moccasin() {
+        return "Moccasin";
+      }
+
+      static const char* const peach_puff() {
+        return "PeachPuff";
+      }
+
+      static const char* const pale_goldenrod() {
+        return "PaleGoldenrod";
+      }
+
+      static const char* const khaki() {
+        return "Khaki";
+      }
+
+      static const char* const dark_khaki() {
+        return "DarkKhaki";
+      }
+
+      static const char* const gold() {
+        return "Gold";
+      }
+
+      static const char* const cornsilk() {
+        return "Cornsilk";
+      }
+
+      static const char* const blanched_almond() {
+        return "BlanchedAlmond";
+      }
+
+      static const char* const bisque() {
+        return "Bisque";
+      }
+
+      static const char* const navajo_white() {
+        return "NavajoWhite";
+      }
+
+      static const char* const wheat() {
+        return "Wheat";
+      }
+
+      static const char* const burly_wood() {
+        return "BurlyWood";
+      }
+
+      static const char* const tan() {
+        return "Tan";
+      }
+
+      static const char* const rosy_brown() {
+        return "RosyBrown";
+      }
+
+      static const char* const sandy_brown() {
+        return "SandyBrown";
+      }
+
+      static const char* const goldenrod() {
+        return "Goldenrod";
+      }
+
+      static const char* const dark_goldenrod() {
+        return "DarkGoldenrod";
+      }
+
+      static const char* const peru() {
+        return "Peru";
+      }
+
+      static const char* const chocolate() {
+        return "Chocolate";
+      }
+
+      static const char* const saddle_brown() {
+        return "SaddleBrown";
+      }
+
+      static const char* const sienna() {
+        return "Sienna";
+      }
+
+      static const char* const brown() {
+        return "Brown";
+      }
+
+      static const char* const maroon() {
+        return "Maroon";
+      }
+
+      static const char* const dark_olive_green() {
+        return "DarkOliveGreen";
+      }
+
+      static const char* const olive() {
+        return "Olive";
+      }
+
+      static const char* const olive_drab() {
+        return "OliveDrab";
+      }
+
+      static const char* const yellow_green() {
+        return "YellowGreen";
+      }
+
+      static const char* const lime_green() {
+        return "LimeGreen";
+      }
+
+      static const char* const lime() {
+        return "Lime";
+      }
+
+      static const char* const lawn_green() {
+        return "LawnGreen";
+      }
+
+      static const char* const chartreuse() {
+        return "Chartreuse";
+      }
+
+      static const char* const green_yellow() {
+        return "GreenYellow";
+      }
+
+      static const char* const spring_green() {
+        return "SpringGreen";
+      }
 //
-//Green colors
-//DarkOliveGreen	55 6B 2F	 85 107  47
-//Olive	80 80 00	128 128   0
-//OliveDrab	6B 8E 23	107 142  35
-//YellowGreen	9A CD 32	154 205  50
-//LimeGreen	32 CD 32	 50 205  50
-//Lime	00 FF 00	  0 255   0
-//LawnGreen	7C FC 00	124 252   0
-//Chartreuse	7F FF 00	127 255   0
-//GreenYellow	AD FF 2F	173 255  47
-//SpringGreen	00 FF 7F	  0 255 127
+//      static const char* const XXX() {
+//        return "XXX";
+//      }
+//
+//      static const char* const XXX() {
+//        return "XXX";
+//      }
+
 //MediumSpringGreen	00 FA 9A	  0 250 154
 //LightGreen	90 EE 90	144 238 144
 //PaleGreen	98 FB 98	152 251 152
